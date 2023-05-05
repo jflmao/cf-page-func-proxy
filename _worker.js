24a35626b7d874c37be54168ed0e706d61fe006d
jflmao <jflmao@live.cn>
2023-05-06 01:12:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="vpn2.0x08.cn";
+        url.hostname="www.google.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
